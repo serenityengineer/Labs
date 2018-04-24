bf4e00dec9aefd69626f1f5618087e8617d74ee2
serenityengineer <38222989+serenityengineer@users.noreply.github.com>
2018-04-23 17:24:14 -0700

    updated

--- Lab 1/swap.s
+++ Lab 1/swap.s
@@ -8,11 +8,11 @@ str2:	.asciz	"\nzing[5] now has "
 
 	.text
 
-main:	
+main:
 	la	a0, zing	#address of zing array
 	li	a1, 4
 	jal	swap
-	
+
 	li	a7, 4		#system call for printing a string
 	la	a0, str1
 	ecall
@@ -24,28 +24,31 @@ main:
 	li	a7, 4		#system call for printing a string
 	la	a0, str2
 	ecall
-	
+
 	li	a7, 1		#system call for printing an integer in ASCII
 	lw	a0, 4(t1)
 	ecall
-	
+
 	li	a7, 11		#system call for printing a character in ASCII
 	li	a0, 10
 	ecall
-	
+
 	li	a7, 10		#system call for an exit
 	ecall
-	
 
-swap:	
+
+swap:
 	slli	t1, a1, 2	#reg t1=k*4
 	add		t1, a0, t1	#reg t1=address of zing[k]
-				
+
 	lw		t0, 0(t1)	#reg t0=zing[k]
 	lw		t2, 4(t1)	#reg t2=zing[k+1]
-				
-	sw		t2, 0(t1)	
-	sw		t0, 4(t1)	
-	ret		
+
+	sw		t2, 0(t1)
+	sw		t0, 4(t1)
+	addi    t4, zero, 5
+	addi	t5, t4, 2
+	add 	t6, t4, t5
+	ret
 
 # END OF PROGRAM
