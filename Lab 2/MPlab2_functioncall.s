e793597d18454d3948ff4d6e56302df9f68d0330
serenityengineer <38222989+serenityengineer@users.noreply.github.com>
2018-04-30 09:29:11 -0700

    need to finish main and get an n

---
+++ Lab 2/MPlab2_functioncall.s
@@ -0,0 +1,30 @@
+
+	.data
+	
+	.text
+
+main:
+	
+	
+
+AddItUp:
+	
+	addi sp, sp, -8 # making space on stack
+	sw t0, 0(sp) # saving i
+	sw t1, 4(sp) #saving j
+	
+	add t0, zero, zero # new i
+	add t1, zero, zero # new x
+	
+	forloop:
+		add, t1, t1, t0
+		addi t1, t1, 1
+		blt t0, a2, forloop
+	add a0, zero, t1 # returning x
+		
+	lw t0, 0(sp) #loading original i
+	lw t1, 4(sp) # loading original j
+	addi sp, sp, 8 # deleting stack spaces
+	j main
+		
+	
