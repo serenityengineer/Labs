5b08d7a7c491c3f8217fc53165bddd86065f12ab
mahaliacp <36903308+mahaliacp@users.noreply.github.com>
2018-04-30 11:06:06 -0700

    Need to figure out the sw for saving the value

--- Lab 2/MPlab2_functioncall.s
+++ Lab 2/MPlab2_functioncall.s
@@ -1,16 +1,19 @@
 
-	.data
+.data
 	myArray:	.word	5, 10
+
 	.text
 
 main:
 	la a0, myArray #load address of array
-	li a1, 4
+	
+    li a1, 1
 	jal AddItUp
+  
 #	add a1, zero, a0 #store a0 new value
 
-#	li a0, 10 #load j into a1
-#	jal AddItUp
+	li a1, 2 
+	jal AddItUp
 #	add a2, zero, a0 #store a0 new value
 	
 AddItUp:
@@ -23,18 +26,16 @@ AddItUp:
 	#sw t0, 0(sp) # saving i
 	#sw t1, 4(sp) #saving j
 	
-	#add t0, zero, zero # new i
+	add t3, zero, zero # new i
 	#add t1, zero, zero # new x
 	
 	forloop:
-		add, t2, t2, t3
-		addi t2, t2, 1
-		blt t3, a2, forloop
-	add a0, zero, t1 # returning x
-		
+		add, t2, t2, t3 #x=x+i
+		addi t2, t2, 1 #x=x+1
+		blt t3, a2, forloop #increment i
+	add a2, zero, t1 # returning x
+		#sw t2, 4(a0)
 	#lw t0, 0(sp) #loading original i
 	#lw t1, 4(sp) # loading original j
 	#addi sp, sp, 8 # deleting stack spaces
-	ret a0
-		
-	
+	ret
